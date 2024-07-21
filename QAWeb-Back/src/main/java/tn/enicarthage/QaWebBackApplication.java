2334dede7c72a6840acfb685dc813e073d84c123
Melek <hajrimelek4@gmail.com>
2024-07-21 09:24:12 +0100

    Init

---
+++ QAWeb-Back/src/main/java/tn/enicarthage/QaWebBackApplication.java
@@ -0,0 +1,13 @@
+package tn.enicarthage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaWebBackApplication.class, args);
+	}
+
+}
